8c37237774141464412ce1f9af1e2b3fd61eb289
ayushkamble <kambleayush518@gmail.com>
2025-04-04 13:21:59 +0530

    add home ui

--- app/src/main/java/com/example/medivault/home.java
+++ app/src/main/java/com/example/medivault/home.java
@@ -1,9 +1,6 @@
 package com.example.medivault;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,28 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    private Button registerButton;
-    private Button loginButton;
-
+public class home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        loginButton=findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, home.class);
-            startActivity(intent);
-        });
-
-
-        registerButton = findViewById(R.id.registerButton);
-        registerButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, register.class);
-            startActivity(intent);
-        });
-
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
